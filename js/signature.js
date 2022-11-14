3bd2044bc41d3ba36cd1c3f54b0da321e0c6c260
Danny Clayton <49194062+DannyC4954@users.noreply.github.com>
2022-11-14 11:54:33 +0000

    Add files via upload

--- js/signature.js
+++ js/signature.js
@@ -13,6 +13,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
   });
 
   ctx.forEach(function(c){
+    // Desktop
     c.canvas.addEventListener('mousedown', function(e){
       startPosition(e, c);
     });
@@ -25,6 +26,23 @@ document.addEventListener('DOMContentLoaded', (event) => {
     });
     c.canvas.addEventListener('mousemove', function(e){
       draw(e, c);
+    });
+    // Mobile -- need setting up and testing on mobile / tablet
+    c.canvas.addEventListener('touchstart', function(e){
+        startPosition(e, c);
+    });
+    c.canvas.addEventListener('touchend', function(e){
+        finishedPostition(c);
+    });
+    c.canvas.addEventListener('touchcancel', function(e){
+        painting = false;
+        c.beginPath();
+        if( c.canvas.parentNode.querySelector('.signature-data').value.length == 0 ){
+            c.canvas.parentNode.querySelector('.signature-indicator').innerHTML = '<span class="pill-error"><i class="fa fa-times"></i></span>';
+        }
+    });
+    c.canvas.addEventListener('touchmove', function(e){
+        draw(e, c);
     });
   });
 
@@ -56,12 +74,19 @@ document.addEventListener('DOMContentLoaded', (event) => {
     var rect = ctx.canvas.getBoundingClientRect();
     var x = e.clientX - rect.left;
     var y = e.clientY - rect.top;
-    ctx.lineWidth = 5;
+    if( e.clientX == undefined ){
+        var x = e.touches[0].clientX - rect.left;;
+        var y = e.touches[0].clientY - rect.top;;
+    } else {
+        var x = e.clientX - rect.left;
+        var y = e.clientY - rect.top;
+    }
+    ctx.lineWidth = 4;
     ctx.lineCap = 'round';
     ctx.lineTo(x, y);
     ctx.stroke();
     ctx.beginPath();
     ctx.moveTo(x, y);
   }
-  
+
 });
