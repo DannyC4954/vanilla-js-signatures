af027fcfaf4abe144fdd28dda62ecb1679300f17
Danny Clayton <49194062+DannyC4954@users.noreply.github.com>
2024-10-01 12:14:14 +0100

    Update signature.js

--- js/signature.js
+++ js/signature.js
@@ -1,92 +1,102 @@
-document.addEventListener('DOMContentLoaded', (event) => {
+const canvas = document.querySelectorAll('.signature-box');
+const containerWidth = document.querySelector('.canvas-container').attributes.class.ownerElement.clientWidth;
+const eventTypes = ['mousedown', 'mouseup', 'mouseout', 'mousemove', 'touchstart', 'touchend', 'touchcancel', 'touchmove'];
+const ctx = [];
+const clearCanvas = document.querySelectorAll('.clear-canvas-btn');
+let painting = false;
 
-  const canvas = document.querySelectorAll('.signature-box');
-  const containerWidth = document.querySelector('.canvas-container').attributes.class.ownerElement.clientWidth;
-  const ctx = [];
-  const clearCanvas = document.querySelectorAll('.clear-canvas-btn');
-  var painting = false;
-
+function prepateSignatureBoxes(){
   canvas.forEach(function(c){
     c.height = 250;
     c.width = containerWidth;
     ctx.push(document.getElementById(c.id).getContext('2d'));
   });
 
-  ctx.forEach(function(c){
-    // Desktop
-    c.canvas.addEventListener('mousedown', function(e){
-      startPosition(e, c);
+  if (ctx.length > 0) {
+    ctx.forEach(function(c){
+      handleMultipleEvents(eventTypes, c);
     });
-    c.canvas.addEventListener('mouseup', function(e){
-      finishedPostition(c);
-    });
-    c.canvas.addEventListener('mouseout', function(e){
-        painting = false;
-        c.beginPath();
-    });
-    c.canvas.addEventListener('mousemove', function(e){
-      draw(e, c);
-    });
-    // Mobile -- need setting up and testing on mobile / tablet
-    c.canvas.addEventListener('touchstart', function(e){
-        startPosition(e, c);
-    });
-    c.canvas.addEventListener('touchend', function(e){
-        finishedPostition(c);
-    });
-    c.canvas.addEventListener('touchcancel', function(e){
-        painting = false;
-        c.beginPath();
-        if( c.canvas.parentNode.querySelector('.signature-data').value.length == 0 ){
-            c.canvas.parentNode.querySelector('.signature-indicator').innerHTML = '<span class="pill-error"><i class="fa fa-times"></i></span>';
-        }
-    });
-    c.canvas.addEventListener('touchmove', function(e){
-        draw(e, c);
-    });
-  });
+  }
 
   clearCanvas.forEach(function(i){
     i.addEventListener('click', function(e){
-      var selectedCanvas = this.parentNode.querySelector('.signature-box');
-      var context = selectedCanvas.getContext('2d');
+      let selectedCanvas = this.parentNode.querySelector('.signature-box');
+      let context = selectedCanvas.getContext('2d');
+
       context.clearRect(0, 0, context.canvas.width, context.canvas.height);
       context.beginPath();
+      
       this.parentNode.querySelector('.signature-data').value = '';
       this.parentNode.querySelector('.signature-indicator').innerHTML = '';
     });
   });
+}
 
-  function startPosition(e, ctx){
+function handleMultipleEvents(eventTypes, ctx){
+  eventTypes.forEach(function(et){
+    ctx.canvas.addEventListener(et, function(e){
+      switch(et){
+        case 'mousedown':
+        case 'touchstart':
+          startPosition(e, ctx);
+          break;
+        case 'mouseup':
+        case 'touchend':
+          finishedPostition(ctx);
+          break;
+        case 'mouseout':
+        case 'touchcancel':
+          painting = false;
+          ctx.beginPath();
+
+          if (ctx.canvas.parentNode.querySelector('.signature-data').value.length == 0){
+            ctx.canvas.parentNode.querySelector('.signature-indicator').innerHTML = '<span class="pill-error"><i class="fa fa-times"></i></span>';
+          }
+          break;
+        case 'mousemove':
+        case 'touchmove':
+          drawSignature(e, ctx);
+          break;
+      }
+    });
+  });
+}
+
+function startPosition(e, ctx){
     painting = true;
-    draw(e, ctx);
-  }
+    drawSignature(e, ctx);
+}
 
-  function finishedPostition(ctx){
+function finishedPostition(ctx){
     painting = false;
     ctx.canvas.parentNode.querySelector('.signature-data').value = ctx.canvas.toDataURL();
     ctx.canvas.parentNode.querySelector('.signature-indicator').innerHTML = '<i class="fas fa-check-circle" style="color: #30b68c;"></i>';
     ctx.beginPath();
-  }
+}
 
-  function draw(e, ctx){
-    if(!painting) return;
-    var rect = ctx.canvas.getBoundingClientRect();
-    var x = e.clientX - rect.left;
-    var y = e.clientY - rect.top;
-    if( e.clientX == undefined ){
-        var x = e.touches[0].clientX - rect.left;;
-        var y = e.touches[0].clientY - rect.top;;
-    } else {
-        var x = e.clientX - rect.left;
-        var y = e.clientY - rect.top;
+function drawSignature(e, ctx){
+    if(!painting) {
+        return
     }
+
+    let rect = ctx.canvas.getBoundingClientRect();
+    let x = e.clientX - rect.left;
+    let y = e.clientY - rect.top;
+
+    if (e.clientX == undefined) {
+        x = e.touches[0].clientX - rect.left;;
+        y = e.touches[0].clientY - rect.top;;
+    } else {
+        x = e.clientX - rect.left;
+        y = e.clientY - rect.top;
+    }
+
     ctx.lineWidth = 4;
     ctx.lineCap = 'round';
     ctx.lineTo(x, y);
     ctx.stroke();
     ctx.beginPath();
     ctx.moveTo(x, y);
-  }
+}
 
-});
+prepateSignatureBoxes();
